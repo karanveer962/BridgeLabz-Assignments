74899f5356b001d5bf0c411a1d9260ff1cce95e0
karanveer962 <karanveer962@gmail.com>
2023-11-30 01:21:04 +0530

    Incorporated UC-6

--- Assign8/MaxFinder.java
+++ Assign8/MaxFinder.java
@@ -2,51 +2,52 @@ import java.util.*;
 
 public class MaxFinder<T extends Comparable<T>> {
 
-    private T x, y, z;
+    private T[] values;
 
-    public MaxFinder(T x, T y, T z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+    public MaxFinder(T... values) {
+        this.values = values;
     }
 
     public T findMaximum() {
-        return MaxFinder.testMaximum(x, y, z);
+        if (values == null || values.length == 0) {
+            throw new IllegalArgumentException("Empty Array!!!");
+        }
+
+        Arrays.sort(values);
+
+        return values[values.length - 1];
     }
 
-    public static <T extends Comparable<T>> T testMaximum(T x, T y, T z) {
-        T max = x;
-
-        if (y.compareTo(max) > 0) {
-            max = y;
+    public static <T extends Comparable<T>> T testMaximum(T... values) {
+        if (values == null || values.length == 0) {
+            throw new IllegalArgumentException("Empty Array!!!");
         }
 
-        if (z.compareTo(max) > 0) {
-            max = z;
-        }
+        Arrays.sort(values);
+        T max = values[values.length - 1];
 
-        printMax(x, y, z, max);
+        printMax(values, max);
 
         return max;
     }
 
-    private static <T> void printMax(T x, T y, T z, T max) {
-        System.out.println("Maximum of " + x + ", " + y + ", " + z + " is " + max);
+    private static <T> void printMax(T[] values, T max) {
+        System.out.println("Maximum of " + Arrays.toString(values) + " is " + max);
     }
 
     public static void main(String[] args) {
-        
-        MaxFinder<Integer> intMax = new MaxFinder<>(3, 2, 1);
+
+        MaxFinder<Integer> intMax = new MaxFinder<>(11, 6, 8, 27, 3);
         Integer intResult = intMax.findMaximum();
         System.out.println("Test Case 1 - Max (Integer): " + intResult);
 
-        
-        MaxFinder<Float> floatMax = new MaxFinder<>(3.6f, 8.9f, 4.1f);
+
+        MaxFinder<Float> floatMax = new MaxFinder<>(3.6f, 8.9f, 4.1f, 5.5f, 2.2f);
         Float floatResult = floatMax.findMaximum();
         System.out.println("Test Case 2 - Max (Float): " + floatResult);
 
-
-        MaxFinder<String> stringMax = new MaxFinder<>("Simon", "Go", "Back");
+       
+        MaxFinder<String> stringMax = new MaxFinder<>("Simon", "Go", "Back", "To", "Your","Zoneland");
         String stringResult = stringMax.findMaximum();
         System.out.println("Test Case 3 - Max (String): " + stringResult);
     }
