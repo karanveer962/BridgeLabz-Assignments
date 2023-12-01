d108b052fcc85c9996479387b3be2df33ee9968a
karanveer962 <karanveer962@gmail.com>
2023-12-01 22:50:29 +0530

    Incorporated UC1, UC2 and UC3

---
+++ Assign9/LinkedList.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+public class LinkedList<T> {
+
+    private static class Node<T> {
+        T data;
+        Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node<T> head;
+
+   
+    public LinkedList() {
+        this.head = null;
+    }
+
+    public void add(T data) {
+        Node<T> newNode = new Node<>(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<T> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+
+    public void display() {
+        Node<T> current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+
+        linkedList.add(56);
+        linkedList.add(30);
+        linkedList.add(70);
+
+        System.out.print(" Linked List: ");
+        linkedList.display();
+
+       
+    }
+}
