00213225fe61c2955c4ef5540f58bf8a825cc4ec
karanveer962 <karanveer962@gmail.com>
2023-12-03 11:27:09 +0530

    Completed UC2 as well

--- Assign10/BST.java
+++ Assign10/BST.java
@@ -48,16 +48,38 @@ public class BST<K extends Comparable<K>> {
             inOrderTraversalRec(root.right);
         }
     }
-   
+    public int size() {
+        return sizeTree(root);
+    }
+
+    private int sizeTree(INode<K> root) {
+        if (root == null) {
+            return 0;
+        }
+        return 1 + sizeTree(root.left) + sizeTree(root.right);
+    }
 
     public static void main(String[] args) {
         BST<Integer> bst = new BST<>();
          bst.add(56);
          bst.add(30);
          bst.add(70);
+         bst.add(22);
+         bst.add(40);
+         bst.add(11);
+         bst.add(3);
+         bst.add(16);
+         bst.add(60);
+         bst.add(95);
+         bst.add(65);
+         bst.add(63);
+         bst.add(67);
          
+         System.out.println("Size of Tree is: "+bst.size());
 
          System.out.println("In-order Traversal:"); //to verify the arrangement
          bst.inOrderTraversal();
+
+
     }
 }
