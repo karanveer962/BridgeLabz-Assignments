996d241bd268a4f3787aab30643c3e95dd94e9b7
karanveer962 <karanveer962@gmail.com>
2023-12-06 00:18:42 +0530

    Password Rule1 satisfied

--- Assign11/Regex.java
+++ Assign11/Regex.java
@@ -18,12 +18,12 @@ public class Regex {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
 
-        System.out.println("Enter Mobile number of the Person: ");
-        String mobNum=sc.next();
+        System.out.println("Enter Password of the Person: ");
+        String password=sc.next();
 
-        Validator MobileNum=new Validator("^[0-9]{2}[0-9]{10}$");
-        if(!MobileNum.Validate(mobNum))
-        System.out.println("Error!!Enter country code followed by 10 digit number.");
+        Validator Password=new Validator("^[a-zA-Z0-9]{8,}$");
+        if(!Password.Validate(password))
+        System.out.println("Error!!Password must be atleast 8 characters.");
         
     }
 }
